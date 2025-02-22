d3ebad73f1f2713ed0fd4d93308193278b8023a4
quentinwarnant <lequentin@gmail.com>
2025-02-22 18:00:07 +0000

    initial setup + initial image initialisation

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+int main()
+{
+    const int16_t img_width = 256;
+    const int16_t img_height = 256;
+
+    std::cout << "P3\n" << img_width << ' ' << img_height << "\n255\n";
+
+    for(int16_t y = 0; y < img_height; ++y)
+    {
+        for(int16_t x = 0; x < img_width; ++x)
+        {
+            auto r = double(x) / (img_width -1);
+            auto g = double(y) / (img_height -1);
+            auto b = 0;
+
+            int ir = int(255.99 * r);
+            int ig = int(255.99 * g);
+            int ib = int(255.99 * b);
+
+            std::cout << ir << ' ' << ig << ' ' << ib << '\n';
+        }
+    }
+}
